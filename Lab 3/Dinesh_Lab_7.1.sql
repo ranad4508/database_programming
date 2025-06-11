3d2c3b4825d3f47767c9a638af0d7fcb235d8132
ranad4508 <rand4508@gmail.com>
2025-06-11 08:58:13 +0545

    lab till 7.1

--- Lab 3/Dinesh_Lab_7.1.sql
+++ Lab 3/Dinesh_Lab_7.1.sql
@@ -24,3 +24,41 @@ EXCEPTION
         DBMS_OUTPUT.PUT_LINE('An error occurred: ' || SQLERRM);
 END;
 /
+
+select * from departments;
+
+/*
+Fetch and assign all the employees of the IT department to a cursor
+*/
+SET SERVEROUTPUT ON;
+
+DECLARE
+    CURSOR cr_employee
+    IS 
+    SELECT * FROM employees
+    WHERE department_id = 60;
+    
+    vr_emp_record cr_employee%ROWTYPE;
+    
+BEGIN
+    -- Open cursor
+    OPEN cr_employee;
+    
+    -- fetch cursor
+    LOOP
+        FETCH cr_employee INTO vr_emp_record;
+        
+        DBMS_OUTPUT.PUT_LINE('Emp ID: '||vr_emp_record.employee_id|| ' Fullname: '||vr_emp_record.last_name || ' '||vr_emp_record.first_name ||' Salary: '||vr_emp_record.salary);
+    END LOOP;
+    
+    -- close cursor
+    CLOSE cr_employee;
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        DBMS_OUTPUT.PUT_LINE('Sorry, no records found');
+
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('An error occurred: ' || SQLERRM);
+COMMIT;
+END;
+/
